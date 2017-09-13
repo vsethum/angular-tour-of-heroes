f061fedcc9c9dd6ffc43c1cd0f6868db4bdebcf8
Vijay Sethumadavan <sethumadavan@gmail.com>
2017-09-13 11:32:13 -0400

    First branch commit

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -13,6 +13,17 @@ var HeroService = (function () {
     HeroService.prototype.getHeroes = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
     };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        var _this = this;
+        return new Promise(function (resolve) {
+            // Simulate server latency with 2 second delay
+            setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
+        });
+    };
     return HeroService;
 }());
 HeroService = __decorate([
--- src/app/heroes.component.js
+++ src/app/heroes.component.js
@@ -9,36 +9,36 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
-var common_1 = require("@angular/common");
 var hero_service_1 = require("./hero.service");
-require("rxjs/add/operator/switchMap");
-var HeroDetailComponent = (function () {
-    function HeroDetailComponent(heroService, route, location) {
+var router_1 = require("@angular/router");
+var HeroesComponent = (function () {
+    function HeroesComponent(heroService, router) {
         this.heroService = heroService;
-        this.route = route;
-        this.location = location;
+        this.router = router;
     }
-    HeroDetailComponent.prototype.ngOnInit = function () {
+    HeroesComponent.prototype.getHeroes = function () {
         var _this = this;
-        this.route.paramMap
-            .switchMap(function (params) { return _this.heroService.getHero(+params.get('id')); })
-            .subscribe(function (hero) { return _this.hero = hero; });
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
     };
-    HeroDetailComponent.prototype.goBack = function () {
-        this.location.back();
+    HeroesComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
     };
-    return HeroDetailComponent;
+    HeroesComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    HeroesComponent.prototype.gotoDetail = function () {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    };
+    return HeroesComponent;
 }());
-HeroDetailComponent = __decorate([
+HeroesComponent = __decorate([
     core_1.Component({
-        selector: 'hero-detail',
-        templateUrl: './hero-detail.component.html',
-        styleUrls: ['./hero-detail.component.css']
+        selector: 'my-heroes',
+        templateUrl: './heroes.component.html',
+        styleUrls: ['./heroes.component.css']
     }),
     __metadata("design:paramtypes", [hero_service_1.HeroService,
-        router_1.ActivatedRoute,
-        common_1.Location])
-], HeroDetailComponent);
-exports.HeroDetailComponent = HeroDetailComponent;
-//# sourceMappingURL=hero-detail.component.js.map
+        router_1.Router])
+], HeroesComponent);
+exports.HeroesComponent = HeroesComponent;
+//# sourceMappingURL=heroes.component.js.map
